44b1707ab5045535a10e4934932e7f267330b2d3
JianpingZeng <z1215jping@163.com>
2016-03-27 14:08:48 +0800

    constant propagation over HIR done well.

---
+++ src/optimization/ConstantProp.java
@@ -0,0 +1,75 @@
+package optimization;
+
+import hir.BasicBlock;
+import hir.Instruction;
+import hir.Method;
+import hir.Value;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+/**
+ * <p>This defines a class which implements constant propgation
+ * and constant folding as subtask.
+ * <p>This file is a member of <a href={@docRoot/optimization}>Machine Indepedence
+ * Optimization</a>.
+ *
+ * @see Canonicalizer
+ * @see DCE
+ * @see GVN
+ * @see UCE
+ *
+ * @author Jianping Zeng
+ *
+ */
+public class ConstantProp
+{
+	private long numsInstKilled = 0;
+	/**
+	 * Performs constant propagation optimization upon given method.
+	 * @param m A method where Optimization performed.
+	 * @return  Whether execution of optimization is successful.
+	 */
+	public boolean runOnMethod(Method m)
+	{
+		LinkedList<Instruction> worklist =
+				new LinkedList<>();
+		// initializes the worklist to all of the instructions ready to
+		// process
+		for (BasicBlock bb : m)
+		{
+			for (Instruction inst : bb)
+				worklist.add(inst);
+		}
+		boolean changed = false;
+		Canonicalizer can = new Canonicalizer();
+		while (!worklist.isEmpty())
+		{
+			Instruction inst = worklist.removeFirst();
+			// ignores it if no other instruction use it
+			if (!inst.isUseEmpty())
+			{
+				Value val = can.constantFoldInstruction(inst);
+
+				if (val != null)
+				{
+					if (val instanceof Value.Constant)
+					{
+						// performs constant propagation
+						for (Instruction user : inst.uses)
+							worklist.addLast(user);
+					}
+					// constant folding and strength reduction
+					inst.replaceAllUsesWith(val);
+					worklist.remove(inst);
+					inst.eraseFromBasicBlock();
+
+					// marks the changed flag
+					changed = true;
+					++numsInstKilled;
+				}
+			}
+		}
+		return changed;
+	}
+}
