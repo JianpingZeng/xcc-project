8e04a684f5010cc2466b2b1fee656b02b098c729
Xlous.zeng <z1215jping@163.com>
2016-12-22 22:13:18 +0800

    refactoring the MachineLoopInfo class

--- src/backend/analysis/MachineLoopInfo.java
+++ src/backend/analysis/MachineLoopInfo.java
@@ -20,6 +20,9 @@ import backend.codegen.MachineBasicBlock;
 import backend.codegen.MachineFunction;
 import backend.codegen.MachineFunctionPass;
 import backend.pass.AnalysisUsage;
+import backend.support.DepthFirstOrder;
+
+import java.util.*;
 
 /**
  * @author Xlous.zeng
@@ -28,10 +31,22 @@ import backend.pass.AnalysisUsage;
 public class MachineLoopInfo extends MachineFunctionPass
     implements LoopInfoBase<MachineBasicBlock, MachineLoop>
 {
+    private HashMap<MachineBasicBlock, MachineLoop> bbMap = new HashMap<>();
+
+    private ArrayList<MachineLoop> topLevelLoops = new ArrayList<>();
+
+    @Override
+    public void getAnalysisUsage(AnalysisUsage au)
+    {
+        assert au != null;
+        au.addRequired(MachineDominatorTree.class);
+        super.getAnalysisUsage(au);
+    }
+
     @Override
     public String getPassName()
     {
-        return "Machine Natural Loop Construction";
+        return "Machine Natural MachineLoop tree Construction";
     }
 
     /**
@@ -44,17 +59,227 @@ public class MachineLoopInfo extends MachineFunctionPass
     @Override
     public boolean runOnMachineFunction(MachineFunction mf)
     {
+        calculate(getAnalysisToUpDate(MachineDominatorTree.class));
         return false;
     }
 
-    public void getAnalysisUsage(AnalysisUsage au)
+    private void calculate(MachineDominatorTree dt)
     {
-        au.addRequired(MachineDominatorTree.class);
-        super.getAnalysisUsage(au);
+        MachineBasicBlock rootNode = dt.getRootNode().getBlock();
+
+        Set<MachineBasicBlock> dfList = DepthFirstOrder.reversePostOrder(rootNode);
+        for (MachineBasicBlock bb : dfList)
+        {
+            MachineLoop loop = considerForLoop(bb, dt);
+            if (loop != null)
+                topLevelLoops.add(loop);
+        }
     }
 
+    private MachineLoop considerForLoop(MachineBasicBlock bb, MachineDominatorTree dt)
+    {
+        if (bbMap.containsKey(bb))
+            return null;
+
+        Stack<MachineBasicBlock> todoStack = new Stack<>();
+        Iterator<MachineBasicBlock> itr = bb.predIterator();
+        while (itr.hasNext())
+        {
+            MachineBasicBlock pred = itr.next();
+            if (dt.dominates(bb, pred))
+                todoStack.push(pred);
+        }
+
+        if (todoStack.isEmpty()) return null;
+
+        MachineLoop l = new MachineLoop(bb);
+        bbMap.put(bb, l);
+
+        MachineBasicBlock entryBlock = bb.getParent().getEntryBlock();
+        while (!todoStack.isEmpty())
+        {
+            MachineBasicBlock cur = todoStack.pop();
+            // The current block is not contained in loop as yet,
+            // and it is reachable from entry block.
+            if (!l.contains(cur) && dt.dominates(entryBlock, cur))
+            {
+                // Check to see if this block already belongs to a loop.  If this occurs
+                // then we have a case where a loop that is supposed to be a child of
+                // the current loop was processed before the current loop.  When this
+                // occurs, this child loop gets added to a part of the current loop,
+                // making it a sibling to the current loop.  We have to reparent this
+                // loop.
+                MachineLoop subLoop = getLoopFor(cur);
+                if (subLoop != null)
+                {
+                    if (subLoop.getHeaderBlock() == cur && isNotAlreadyContainedIn(subLoop, l))
+                    {
+                        assert subLoop.getParentLoop() != null && subLoop.getParentLoop() != l;
+                        MachineLoop subParentLoop = subLoop.outerLoop;
+                        assert subParentLoop.getSubLoops().contains(subLoop);
+                        subParentLoop.subLoops.remove(subLoop);
+
+                        subLoop.setParentLoop(l);
+                        l.subLoops.add(subLoop);
+                    }
+                }
+
+                l.blocks.add(cur);
+                for (Iterator<MachineBasicBlock> predItr = cur.predIterator();
+                     predItr.hasNext();)
+                {
+                    todoStack.push(predItr.next());
+                }
+            }
+        }
+
+        // If there are any loops nested within this loop, create them.
+        for (MachineBasicBlock block : l.blocks)
+        {
+            MachineLoop newLoop = considerForLoop(block, dt);
+            if (newLoop != null)
+            {
+                l.subLoops.add(newLoop);
+                newLoop.setParentLoop(l);
+            }
+        }
+
+        // Add the basic blocks that comprise this loop to the BBMap so that this
+        // loop can be found for them.
+        for (MachineBasicBlock block : l.blocks)
+        {
+            if (!bbMap.containsKey(block))
+            {
+                bbMap.put(block, l);
+            }
+        }
+
+        HashMap<MachineBasicBlock, MachineLoop> containingLoops = new HashMap<>();
+        for (int i = 0; i < l.subLoops.size(); i++)
+        {
+            MachineLoop childLoop = l.subLoops.get(i);
+            assert childLoop.getParentLoop() == l;
+
+            MachineLoop containedLoop;
+            if ((containedLoop = containingLoops.get(childLoop.getHeaderBlock())) != null)
+            {
+                moveSiblingLoopInto(childLoop, containedLoop);
+                --i;
+            }
+            else
+            {
+                for (int b = 0, e = childLoop.blocks.size(); b < e; b++)
+                {
+                    MachineLoop blockLoop = containingLoops.get(childLoop.blocks.get(i));
+                    if (blockLoop == null)
+                        blockLoop = childLoop;
+                    else if (blockLoop != childLoop)
+                    {
+                        MachineLoop subLoop = blockLoop;
+                        for (int j = 0, sz = subLoop.blocks.size(); j < sz; j++)
+                        {
+                            containingLoops.put(subLoop.blocks.get(j), childLoop);
+
+                            moveSiblingLoopInto(subLoop, childLoop);
+                            --i;
+                        }
+                    }
+                }
+            }
+        }
+
+        return l;
+    }
+
+    /**
+     * This method moves the newChild loop to live inside of the newParent,
+     * instead of being a slibing of it.
+     * @param newChild
+     * @param newParent
+     */
+    private void moveSiblingLoopInto(MachineLoop newChild, MachineLoop newParent)
+    {
+        MachineLoop oldParent = newChild.getParentLoop();
+        assert oldParent != null && oldParent == newParent.getParentLoop();
+
+        assert oldParent.subLoops.contains(newChild) :"Parent field incorrent!";
+        oldParent.subLoops.remove(newChild);
+        newParent.subLoops.add(newChild);
+        newChild.setParentLoop(null);
+
+        insertLoopInto(newChild, newParent);
+    }
+
+    private void insertLoopInto(MachineLoop child, MachineLoop parent)
+    {
+        MachineBasicBlock header = child.getHeaderBlock();
+        assert parent.contains(header) : "This loop should not be inserted here";
+
+        // Check to see if it belongs in a child loop...
+        for (int i = 0, e = parent.subLoops.size(); i < e; i++)
+        {
+            if (parent.subLoops.get(i).contains(header))
+            {
+                insertLoopInto(child, parent.subLoops.get(i));
+                return;
+            }
+        }
+
+        parent.subLoops.add(child);
+        child.setParentLoop(parent);
+    }
+
+    private boolean isNotAlreadyContainedIn(MachineLoop subLoop, MachineLoop parentLoop)
+    {
+        if (subLoop == null) return true;
+        if (subLoop == parentLoop) return false;
+        return isNotAlreadyContainedIn(subLoop.getParentLoop(), parentLoop);
+    }
+
+    @Override
+    public HashMap<MachineBasicBlock, MachineLoop> getBBMap()
+    {
+        return bbMap;
+    }
+
+    @Override
+    public ArrayList<MachineLoop> getTopLevelLoop()
+    {
+        return topLevelLoops;
+    }
+
+    @Override
+    public int getLoopDepth(MachineBasicBlock bb)
+    {
+        MachineLoop ml = getLoopFor(bb);
+        return ml != null ? ml.getLoopDepth() : 0;
+    }
+
+    @Override
+    public boolean isLoopHeader(MachineBasicBlock bb)
+    {
+        MachineLoop ml = getLoopFor(bb);
+        return ml != null && ml.getHeaderBlock() == bb;
+    }
+
+    @Override
+    public void ensureIsTopLevel(MachineLoop loop, String msg)
+    {
+        assert loop.getParentLoop() == null : msg;
+    }
+
+    @Override
     public void removeBlock(MachineBasicBlock mbb)
     {
-
+        if (bbMap.containsKey(mbb))
+        {
+            MachineLoop loop = bbMap.get(mbb);
+            while(loop != null)
+            {
+                loop.removeBlockFromLoop(mbb);
+                loop = loop.getParentLoop();
+            }
+            bbMap.remove(mbb);
+        }
     }
 }
