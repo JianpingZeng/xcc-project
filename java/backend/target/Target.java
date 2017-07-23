0b3d782f14f8e2c34813f6526cff78496d9e6f85
Jianping Zeng <z1215jping@163.com>
2017-07-24 00:18:31 +0800

    finish TargetMachine and AsmPrinter registry

--- java/backend/target/Target.java
+++ java/backend/target/Target.java
@@ -16,6 +16,12 @@ package backend.target;
  * permissions and limitations under the License.
  */
 
+import backend.codegen.AsmPrinter;
+import jlang.system.Triple;
+import jlang.system.Triple.ArchType;
+
+import java.io.OutputStream;
+
 /**
  * Wrapper for Target specific information.
  * <p>
@@ -32,5 +38,182 @@ package backend.target;
  */
 public class Target
 {
+    public interface AsmInfoCtor
+    {
+        TargetAsmInfo create(Target t, String triple);
+    }
 
+    public interface TargetMachineCtor
+    {
+        TargetMachine create(Target t, String triple, String features);
+    }
+
+    public interface AsmPrinterCtor
+    {
+        AsmPrinter create(OutputStream os,
+                TargetMachine tm,
+                TargetAsmInfo asmInfo,
+                boolean verbose);
+    }
+
+    private Target next;
+
+    public static Target firstTarget;
+
+    private TripleMatcher tripleMatchQualityFn;
+
+    private String name;
+
+    /**
+     * A short desciption.
+     */
+    private String shortDesc;
+
+    private AsmInfoCtor asmInfoCtor;
+
+    /**
+     * Construction function for this target's TargetMachine, if registered.
+     */
+    private TargetMachineCtor targetMachineCtor;
+    /**
+     * Construction function for this target's Assembly Printer, if registered.
+     */
+    private AsmPrinterCtor asmPrinterCtor;
+
+    /**
+     * Create a TargetAsmInfo implementation for the specified
+     * target triple.
+     *
+     * @param triple  This argument is used to determine the target machine
+     * feature set; it should always be provided. Generally this should be
+     * either the target triple from the module, or the target triple of the
+     * host if that does not exist.
+     * @return
+     */
+    public TargetAsmInfo createAsmInfo(String triple)
+    {
+        if (asmInfoCtor == null)
+            return null;
+        return asmInfoCtor.create(this, triple);
+    }
+
+    /**
+     * Create a target specific machine implementation for the specified {@code triple}
+     * @param triple    This argument is used to determine the target machine
+     * feature set; it should always be provided. Generally this should be
+     * either the target triple from the module, or the target triple of the
+     * host if that does not exist.
+     * @param features
+     * @return
+     */
+    public TargetMachine createTargetMachine(String triple, String features)
+    {
+        if (targetMachineCtor == null)
+            return null;
+        return targetMachineCtor.create(this, triple, features);
+    }
+
+    /**
+     * Create a target specific assembly printer pass.
+     * @param os
+     * @param tm
+     * @param asmInfo
+     * @param verbose
+     * @return
+     */
+    public AsmPrinter createAsmPrinter(OutputStream os,
+            TargetMachine tm,
+            TargetAsmInfo asmInfo,
+            boolean verbose)
+    {
+        if (asmPrinterCtor == null)
+            return null;
+        return asmPrinterCtor.create(os, tm, asmInfo, verbose);
+    }
+
+    /**
+     * A interface to define what is the best quality matcher to select a best
+     * suitable target for specified triple string.
+     */
+    public interface TripleMatcher
+    {
+        int apply(String triple);
+    }
+
+    /**
+     * A target registry factory.
+     * @author Xlous.zeng
+     * @version 0.1
+     */
+    public static class TargetRegistry
+    {
+        public static void registerTargetMachine(Target target, TargetMachineCtor ctor)
+        {
+            if (target.targetMachineCtor != null)
+                return;
+            target.targetMachineCtor = ctor;
+        }
+
+        public static void registerAsmInfo(Target target, AsmInfoCtor ctor)
+        {
+            if (target.asmInfoCtor != null)
+                return;
+            target.asmInfoCtor = ctor;
+        }
+
+        public static void registerAsmPrinter(Target target, AsmPrinterCtor ctor)
+        {
+            if (target.asmPrinterCtor != null)
+                return;
+            target.asmPrinterCtor = ctor;
+        }
+
+        private static void registerTarget(Target t,
+                String name,
+                String shortDesc,
+                TripleMatcher matcher,
+                boolean hasJIT)
+        {
+            assert name != null && !name.isEmpty() && matcher != null:
+                    "Missing required target information!";
+
+            // Check if this target has already been initialized, we allow this as a
+            // convenience to some clients.
+            if (t.name != null)
+                return;
+
+            t.next = firstTarget;
+            firstTarget = t;
+            t.name = name;
+            t.shortDesc = shortDesc;
+            t.tripleMatchQualityFn = matcher;
+        }
+
+        public static void registerTarget(Target t,
+                String name,
+                String shortDesc,
+                ArchType archType,
+                boolean hasJIT)
+        {
+            registerTarget(t, name, shortDesc, new GetTripleMatchQuality(archType), hasJIT);
+        }
+
+        public static class GetTripleMatchQuality implements TripleMatcher
+        {
+            private ArchType archType;
+
+            public GetTripleMatchQuality(ArchType ty)
+            {
+                archType = ty;
+            }
+
+            @Override
+            public int apply(String tt)
+            {
+                if (new Triple(tt).getArch() == archType)
+                    return 20;
+                return 0;
+            }
+        }
+    }
 }
---
+++ java/backend/target/x86/X86TargetInfo.java
@@ -0,0 +1,84 @@
+package backend.target.x86;
+/*
+ * Xlous C language Compiler
+ * Copyright (c) 2015-2017, Xlous Zeng.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import backend.target.Target;
+import backend.target.Target.TargetRegistry;
+import jlang.system.Triple;
+
+/**
+ * @author Xlous.zeng
+ * @version 0.1
+ */
+public class X86TargetInfo
+{
+    public static Target theX86_32Target = new Target();
+    public static Target theX86_64Target = new Target();
+
+    public static void InitializeX86TargetInfo()
+    {
+        TargetRegistry.registerTarget(theX86_32Target, "x86",
+                "32-bit X86: Pentium-Pro and above",
+                Triple.ArchType.x86, false);
+
+        TargetRegistry.registerTarget(theX86_64Target, "x86-64",
+                "64-bit X86: EM64T and AMD64",
+                Triple.ArchType.x86_64, false);
+    }
+
+    /**
+     * A function interface variable to create X86 32bit target machine.
+     */
+    public static Target.TargetMachineCtor X86_32TargetMachineMaker =
+            (t, triple, features) -> new X86_32TargetMachine(t, triple, features);
+
+    /**
+     * A function interface variable to create X86 64 bit target machine.
+     */
+    public static Target.TargetMachineCtor X86_64TargetMachineMaker =
+            (t, triple, features) -> new X86_64TargetMachine(t, triple, features);
+
+    public static Target.AsmInfoCtor createTargetAsmInfo = (t, triple) ->
+    {
+        Triple theTriple = new Triple(triple);
+        switch (theTriple.getOS())
+        {
+            default: return new X86ELFTargetAsmInfo(theTriple);
+        }
+    };
+
+    public static Target.AsmPrinterCtor createX86AsmPrinter = (os, tm, asmInfo, verbose) ->
+    {
+        /*
+        FIXME currently only supporting ATT syntax assembler.
+        if (tm.getTargetAsmInfo().getAssemblerDialect() == 1)
+            return new X86IntelAsmPrinter(os, tm, asmInfo, verbose);
+        */
+        return X86ATTAsmPrinter.createX86AsmCodeEmitter(os, (X86TargetMachine) tm);
+    };
+
+    public static void LLVMInitiliazeX86Target()
+    {
+        TargetRegistry.registerTargetMachine(theX86_32Target, X86_32TargetMachineMaker);
+        TargetRegistry.registerTargetMachine(theX86_64Target, X86_64TargetMachineMaker);
+
+        TargetRegistry.registerAsmInfo(theX86_32Target, createTargetAsmInfo);
+        TargetRegistry.registerAsmInfo(theX86_64Target, createTargetAsmInfo);
+
+        TargetRegistry.registerAsmPrinter(theX86_32Target, createX86AsmPrinter);
+    }
+}
