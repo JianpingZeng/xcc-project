29c9cd38818f6b7d576f09886ac1db35bf28387c
xlous.zeng <z1215jping@163.com>
2017-10-19 23:41:47 +0800

    fix bug of duplicate newline character in emitting diagnostic

---
+++ java/jlang/test/Lexer/test.c
@@ -0,0 +1,33 @@
+#include "test.h"
+//#include <stdio.h>
+
+static int countItr = 0;
+float ffabs(float x)
+{
+	return ((x >= 0) << 1)*x - x;
+}
+float fsqrt(float a)
+{
+	float x1 = 0.8f;
+	float x2 = 0.0f;
+	while(1) 
+	{
+		x2 = (a / x1 + x1) / 2.0;
+		++countItr;
+		if (ffabs(x1 - x2) <= 0.001)
+		   break;	
+		x1 = x2;
+	}
+
+	return x2;
+}
+
+int main()
+{
+	float x = 2.0;
+	countItr = 0;
+	fsqrt(x);
+	//printf("the sqrt of %f is %f.\n", x, );
+	//printf("the count of iteration is %d.\n", countItr);
+	return 0;
+}
---
+++ java/jlang/test/Lexer/test.h
@@ -0,0 +1,14 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+/**
+ * Compute the abstract value of the given float point number.
+ */ 
+float ffabs(float x);
+/**
+ * Compute the squere root of the specified float number by using
+ * Newton iteration method.
+ */ 
+float fsqrt(float a);
+
+#endif
